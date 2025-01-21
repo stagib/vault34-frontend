18361b3538ae07435cc547adcb1826f9e8e73d76
stagib <147700418+stagib@users.noreply.github.com>
2025-01-21 16:01:56 +1100

    feat: fetch post on home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import { API_URL } from '$lib/config';
+	import { onMount } from 'svelte';
+
+	let posts = $state([]);
+	let page = $state(1);
+	let hasNext = $state(true);
+	let loading = $state(false);
+	let target = $state(null);
+
+	async function fetchPosts() {
+		if (loading || !hasNext) return;
+		loading = true;
+
+		try {
+			const response = await fetch(`${API_URL}/posts?page=${page}`);
+			const data = await response.json();
+
+			if (response.ok) {
+				posts = [...posts, ...data.items];
+				hasNext = data.page < data.pages;
+				page = page + 1;
+			}
+		} catch (error) {
+			throw error;
+		}
+		loading = false;
+	}
+
+	onMount(() => {
+		fetchPosts();
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				if (entry.isIntersecting) {
+					fetchPosts();
+				}
+			},
+			{ root: null, threshold: 0, rootMargin: '100px' }
+		);
+
+		if (hasNext) observer.observe(target);
+		return () => observer.disconnect;
+	});
+</script>
+
+<div class="columns-6 gap-4">
+	{#each posts as post}
+		{#if post.thumbnail}
+			<a href={`/posts/${post.id}`}>
+				<img src={post.thumbnail} alt="" />
+			</a>
+		{/if}
+	{/each}
+</div>
+
+{#if hasNext}
+	<div bind:this={target}></div>
+{/if}

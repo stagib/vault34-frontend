659ab08a08da6becd9f27120ab0b6e81deeb7bd8
stagib <147700418+stagib@users.noreply.github.com>
2025-02-05 19:12:56 +1100

    style: make vault page more similar to post page

--- src/routes/vault/[id]/+page.svelte
+++ src/routes/vault/[id]/+page.svelte
@@ -14,19 +14,22 @@
 
 {#if vault}
 	<div class="flex h-screen px-4 pb-10 pt-14">
-		<div class="flex max-h-full w-full max-w-xs flex-col">
-			<div class="flex border border-zinc-600 bg-zinc-800 px-4 py-4 pb-8">
-				<div class="flex flex-col">
+		<div class="flex max-h-full w-full max-w-72 flex-col">
+			<div class="flex border border-zinc-600 bg-zinc-800 py-2 pb-8">
+				<div class="flex flex-col px-4">
 					<div class="mb-4 text-lg font-semibold">{vault.title}</div>
-					<div class="mb-1 text-sm text-zinc-300">created {vault.time_since}</div>
+					<div class="mb-1 flex items-center gap-2">
+						<div class="text-sm font-semibold">{vault.user.username}</div>
+						<div class="text-xs text-zinc-300">{vault.time_since}</div>
+					</div>
 					<div class="flex items-center gap-2">
-						<div class="text-sm text-zinc-300">{vault.post_count} posts</div>
-						<div class="text-sm text-zinc-300">|</div>
-						<div class="text-sm text-zinc-300">{vault.privacy}</div>
+						<div class="text-xs text-zinc-300">{vault.post_count} posts</div>
+						<div class="text-xs text-zinc-300">|</div>
+						<div class="text-xs text-zinc-300">{vault.privacy}</div>
 					</div>
 				</div>
 
-				<div class="ml-auto">
+				<div class="ml-auto px-3">
 					<MoreButtonVault {user} {vault} />
 				</div>
 			</div>

b7791889522d461c2f4d2ce0fb09e39c80e9a337
stagib <147700418+stagib@users.noreply.github.com>
2025-02-17 12:05:31 +1100

    style: navbar changes

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -45,46 +45,49 @@
 	}
 </script>
 
-<div class="fixed top-0 z-50 w-full bg-zinc-950 px-2 sm:px-4">
-	<div class="z-50 flex w-full items-center justify-between rounded-b-md bg-zinc-800 py-1">
+<div class="fixed top-0 z-50 w-full border-b border-zinc-700 bg-zinc-950 px-4">
+	<div class="z-50 flex w-full items-center justify-between rounded-b-md py-2">
 		<div class="flex text-sm">
-			<a class="bg-zinc-800 px-4 py-1" href="/">Vault34</a>
-			<a class="bg-zinc-800 px-4 py-1" href="/create">Create</a>
-			<div class="relative mx-4" use:clickOutside={() => (showResults = false)}>
-				<TextInput
-					className={'w-80 bg-zinc-800 px-2 py-1 rounded-md outline-none border border-zinc-700'}
-					placeholder={'Search'}
-					name={'Query'}
-					minLength={1}
-					maxLength={30}
-					onInput={searchTags}
-					onClick={() => (showResults = true)}
-					onKeypress={handleKeyPress}
-					bind:this={queryInput}
-					bind:value={queryValue}
-				/>
+			<a class="bg-transparent px-4 py-1" href="/">Vault34</a>
+			<a class="bg-transparent px-4 py-1 text-zinc-200" href="/create">Create</a>
+		</div>
 
-				{#if showResults}
-					{#if searchResults.length > 0}
-						<div class="absolute top-full z-50 w-full gap-2 bg-zinc-800 py-2">
-							{#each searchResults as tag}
-								<a href={`/?query=${tag.name}`} onclick={() => tagClick(tag.name)}>
-									<div class="flex items-center gap-2 p-2 hover:bg-zinc-700">
-										<div class="text-xs">{tag.name}</div>
-										<div class="text-xs text-zinc-400">{tag.count}</div>
-									</div>
-								</a>
-							{/each}
-						</div>
-					{/if}
+		<div class="relative mx-2 w-full" use:clickOutside={() => (showResults = false)}>
+			<TextInput
+				className={'w-full bg-zinc-900 px-2 py-1 rounded-md outline-none border border-zinc-700'}
+				placeholder={'Search'}
+				name={'Query'}
+				minLength={1}
+				maxLength={30}
+				onInput={searchTags}
+				onClick={() => (showResults = true)}
+				onKeypress={handleKeyPress}
+				bind:this={queryInput}
+				bind:value={queryValue}
+			/>
+
+			{#if showResults}
+				{#if searchResults.length > 0}
+					<div class="absolute top-full z-50 w-full gap-2 rounded-md bg-zinc-800 py-2">
+						{#each searchResults as tag}
+							<a href={`/?query=${tag.name}`} onclick={() => tagClick(tag.name)}>
+								<div class="flex items-center gap-2 p-2 hover:bg-zinc-700">
+									<div class="text-xs">{tag.name}</div>
+									<div class="text-xs text-zinc-400">{tag.count}</div>
+								</div>
+							</a>
+						{/each}
+					</div>
 				{/if}
-			</div>
+			{/if}
 		</div>
 
 		{#if user}
-			<a class="bg-zinc-800 px-4 py-1 text-sm" href={`/user/${user.username}`}>Profile</a>
+			<a class="bg-transparent px-4 py-1 text-sm text-zinc-200" href={`/user/${user.username}`}
+				>Profile</a
+			>
 		{:else}
-			<a class="bg-zinc-800 px-4 py-1 text-sm" href="/login">Login</a>
+			<a class="bg-transparent px-4 py-1 text-sm text-zinc-200" href="/login">Login</a>
 		{/if}
 	</div>
 </div>
